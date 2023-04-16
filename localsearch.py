542eda91f8714535ef42ecd1fe40abd408632f43
DANU011 <122770856+DANU011@users.noreply.github.com>
2023-04-16 23:37:12 +0900

    0416

--- localsearch.py
+++ localsearch.py
@@ -7,7 +7,7 @@ def create_problem(filename): # 파이썬은 함수명에 대문자써서 붙여
     # 1-1. 파일을 읽자
     ini_file = open(filename, 'r')
     expression = ini_file.readline()
-    # 1-2. 수식과 리트트로 분리
+    # 1-2. 수식과 리스트로 분리
     # 1-3. 리턴!!
     return expression, # domain
     # pass
